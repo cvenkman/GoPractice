23177937f2e287e819b2e809099363a7a87c158a
Carlos Venkman <cvenkman@si-f3.kzn.21-school.ru>
2022-04-29 00:48:50 +0300

    add deadlock

--- goroutines/main.go
+++ goroutines/main.go
@@ -6,44 +6,61 @@ import (
 )
 
 func foo() {
-	time.Sleep(time.Second)
-	fmt.Println("foo")
-}
-
-/*
-При отправлении значения в небуферизованный канал отправляющая горутина блокируется до тех пор,
-пока другая горутина не выполнит получение из этого канала. После этого обе горутины продолжают работать.
-Верно и обратное, если горутина получает значение, она блокируется, пока значение не будет получено.
-*/
-
-func main() {
-	start := time.Now()
-	
-	// time.Sleep(time.Second)
-	// fmt.Println("main")
-	// go func() {
-	// 	time.Sleep(time.Second)
-	// 	fmt.Println("goroutine")
-	// } ()
 	channel := make(chan string, 5)
-	// num = <- channel
-	go fmt.Println(len(channel), cap(channel)) // 0 5
+	
+	fmt.Println(len(channel), cap(channel)) // 0 5
 	channel <- "cat"
 	channel <- "dog"
-	go fmt.Println(len(channel), cap(channel)) // 1 5
+	fmt.Println(len(channel), cap(channel)) // 2 5
+
 	var str string
 	str = <- channel
-	go fmt.Println(len(channel), cap(channel), str) // 0 5
-	go fmt.Println(len(channel), cap(channel), <- channel) // 0 5
+	fmt.Println(len(channel), cap(channel), str) // 1 5
+	fmt.Println(len(channel), cap(channel), <- channel) // 1 5
+
 	close(channel)
 
 	val, ok := <- channel
 	if ok == true {
 		fmt.Println("--", len(channel), cap(channel), val) // 0 5
-	} 
+	}
+}
 
-	// go foo()
+func main() {
+	start := time.Now()
+	
+	go foo()
+
+	go func() {
+		channel := make(chan int, 2)
+		channel <- 2 + 2
+	
+		go func (c chan int) {
+			fmt.Println("- ", <- c)
+		}(channel)
+	
+		close(channel)
+	}()
+
 	duration := time.Since(start)
 	time.Sleep(time.Second)
 	fmt.Println(duration)
+}
+
+func deadlock() {
+    fmt.Println("main() started")
+
+	/*
+	Deadlock:
+		c := make(chan string)
+		c <- "John"
+	т.к. main - горутина, которая эксклюзивно производит операции с каналом.
+	*/
+
+    go func() {
+		c := make(chan string)
+		c <- "John"
+	}()
+
+    fmt.Println("main() stopped")
 }
